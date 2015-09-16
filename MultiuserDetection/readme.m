6379f0bd0aa77186360befe06df8ad660178796d
Francisco J. R. Ruiz <franrruiz@tsc.uc3m.es>
2015-09-16 09:46:13 +0200

    added comment about GSL

--- MultiuserDetection/readme.m
+++ MultiuserDetection/readme.m
@@ -6,5 +6,6 @@
 % Please make sure that you compile using mex the file "pgas_C.cpp" (or
 % "pgas_C_parallel.cpp if you intend to use several cores using
 % OpenMP) under the folder ./code/pgas/mex. Move the compiled file
-% to ./code/pgas/compiled
+% to ./code/pgas/compiled. Make sure that you use the -lgsl option to
+% compile, since the code makes use of the GSL library
 % 
